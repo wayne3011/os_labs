06fde56a469a883ae3b5a8705f9fb19fa7b7529c
Maksim Ermilov <maks.ermilov.03@mail.ru>
2024-02-22 00:28:54 +0300

    Доделал архивацию

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 #include "argument_parser.h"
 
 int dir_bypass(char *root_dir, char *next_dir, int output_descriptor);
-long get_file_size(int descriptor);
-int archive_folder(char *folder_name, int output_descriptor, DIR *folder);
+long get_file_size(char *path_to_file);
+int archive_folder(char *folder_name, int output_descriptor);
 int archive_file(int output_descriptor, int input_descriptor);
 int main(int argc, char* argv[]) {
     Flags flags = {0};
@@ -27,13 +27,14 @@ int main(int argc, char* argv[]) {
         printf("path: %s\n", file_names);
     }
     int file_flags;
-    if(access(output_path, F_OK)) file_flags = O_CREAT | S_IWRITE | O_RDONLY;
-    else file_flags = S_IWRITE | O_RDONLY;
+    if(access(output_path, F_OK)) file_flags = O_CREAT | S_IWRITE | S_IREAD | O_WRONLY;
+    else file_flags = S_IWRITE | S_IREAD  | O_WRONLY;
     int output_descriptor = open(output_path, file_flags);
     if(output_descriptor < 0) {
         err(1, "%s", file_names);
     }
     dir_bypass(file_names, "", output_descriptor);
+    close(output_descriptor);
 }
 
 int dir_bypass(char *root_dir, char *next_dir, int output_descriptor) {
@@ -45,7 +46,7 @@ int dir_bypass(char *root_dir, char *next_dir, int output_descriptor) {
     DIR* current_dir = opendir(current_path);
     if(current_dir == NULL)
         return 0;
-    archive_folder(current_path, output_descriptor, current_dir);
+    archive_folder(current_path, output_descriptor);
     struct dirent* sub_dir;
     while((sub_dir = readdir(current_dir)) != NULL) {
         if(sub_dir->d_type == DT_DIR && strcmp(sub_dir->d_name, ".") != 0
@@ -53,16 +54,19 @@ int dir_bypass(char *root_dir, char *next_dir, int output_descriptor) {
             char current_path_dir[4097] = "";
             strcat(current_path_dir, current_path);
             strcat(current_path_dir, sub_dir->d_name);
-            dir_bypass(current_path, sub_dir->d_name, 0);
+            dir_bypass(current_path, sub_dir->d_name, output_descriptor);
         }
     }
     closedir(current_dir);
     return 0;
 }
 
-int archive_folder(char *folder_name, int output_descriptor, DIR *folder) {
+int archive_folder(char *folder_name, int output_descriptor) {
     struct dirent* sub_dir;
-
+    DIR * folder = opendir(folder_name);
+    if(folder == NULL) {
+        err(1, "%s", folder_name);
+    }
     char delimiter = ' ';
     while((sub_dir = readdir(folder)) != NULL) {
         if(sub_dir->d_type != DT_DIR && strcmp(sub_dir->d_name, ".") != 0
@@ -71,28 +75,30 @@ int archive_folder(char *folder_name, int output_descriptor, DIR *folder) {
             strcat(current_path_dir, folder_name);
             strcat(current_path_dir, sub_dir->d_name);
 
-            long file_size = get_file_size(output_descriptor);
+            long file_size = get_file_size(current_path_dir);
             if(file_size == -1) {
                 return -1;
             }
+
             write(output_descriptor, &file_size, sizeof (long ));
             write(output_descriptor, &delimiter, sizeof (char));
             write(output_descriptor, &current_path_dir, strlen(current_path_dir) * sizeof (char));
             write(output_descriptor, &delimiter, sizeof (char));
-            printf("%s", current_path_dir);
+
             int input_descriptor = open(current_path_dir, O_RDONLY);
-            if(input_descriptor < 0) {
+            if(input_descriptor < 0){
                 err(1, "%s", current_path_dir);
             }
             archive_file(output_descriptor, input_descriptor);
             write(output_descriptor, &delimiter, sizeof (char));
         }
     }
+    closedir(folder);
     return 0;
 }
 
-long get_file_size(int descriptor) {
-    FILE* file = fdopen(descriptor, "rb");
+long get_file_size(char *path_to_file) {
+    FILE* file = fopen(path_to_file, "rb");
     if(file == NULL) {
         return -1;
     }
@@ -114,14 +120,16 @@ int archive_file(int output_descriptor, int input_descriptor) {
 
     if(buf == NULL) return -1;
 
-    ssize_t nr, nw;
+    ssize_t nr = 0, nw = 0;
 
     while((nr = read(input_descriptor, buf, bsize)) > 0){
-        for (int off = 0; nr; nr -= nw, off += nw) {
+        for (int off = 0; nr > 0 && nw != -1; nr -= nw, off += nw) {
             nw = write(output_descriptor, buf + off, (size_t)nr);
         }
     }
-
+    if(nr == -1 || nw == -1) {
+        err(1, "");
+    };
     if(buf != NULL) {
         free(buf);
         buf = NULL;
